885b1cd4e65cdfd945a83b775cfb7ca557c1e692
Mfenix1 <149266308+Mfenix1@users.noreply.github.com>
2023-10-28 21:34:12 -0500

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
+
+import React from 'react';
+import '../sass/app.scss';
+
+export default ({ Component, pageProps }) => <Component {...pageProps} />;
