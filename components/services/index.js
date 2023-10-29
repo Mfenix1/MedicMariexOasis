486db9567c181bec5c47dff29c9df52d6744581c
Mfenix1 <marcus.g.pranga@vanderbilt.edu>
2023-10-29 02:08:01 -0500

    Message

--- components/services/index.js
+++ components/services/index.js
@@ -1,16 +1,31 @@
-import Blog from './blog';
-// import Contents from './contents';
-import Footer from './footer';
-import Hero from './hero';
-import Navbar from './navbar';
-import Quotes from './quotes';
-import Services from './services';
+import React from 'react';
 
-export {
-  Blog,
-  Footer,
-  Hero,
-  Navbar,
-  Quotes,
-  Services,
-};
+const Services = () => (
+  <section className="services" id="services">
+    <article className="services__heading">
+      <h2>What can you ask Medic Marie?</h2>
+      <p>
+        To chat with Medic Marie via SMS, text <span class="contact-info">"INFO"</span> to <span class="contact-info">21588046</span> or <span class="contact-info">25658046</span>, and Medic Marie will be ready to guide you with the following:
+      </p>
+    </article>
+    <div className="services__cards">
+      <div className="services__card">
+        <img src="./assets/search-doctor.svg" alt="search doctor" />
+        <h4>Doctor-finder</h4>
+        <p>Medic Marie can list down the possible doctors in your area to treat your illness.</p>
+      </div>
+      <div className="services__card">
+        <img src="./assets/online-pharmacy.svg" alt="online pharmacy" />
+        <h4>Prescriptions & Pharmacies</h4>
+        <p>She also tells you the over-the-counter drug you need and where to find it.</p>
+      </div>
+      <div className="services__card">
+        <img src="./assets/consultation.svg" alt="consultation" />
+        <h4>Health Consultations</h4>
+        <p>Free consultation with our trusted doctors and get the best recomendations</p>
+      </div>
+    </div>
+  </section>
+);
+
+export default Services
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 
-import {
-  Services, Quotes, Hero, Navbar, Footer,
-} from '../components';
+import Services from '../components/services/index';
+import Navbar from '../components/navbar/index';
+import Hero from '../components/hero/index';
+import Quotes from '../components/quotes/index';
+import Footer from '../components/footer/index';
 
 export default () => (
   <div>
