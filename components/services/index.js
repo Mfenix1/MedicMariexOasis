7c00529a657d73a39d3e61814ce06b462a1bdec9
Mfenix1 <149266308+Mfenix1@users.noreply.github.com>
2023-10-28 19:19:58 -0500

    Create index.js

---
+++ components/services/index.js
@@ -0,0 +1,16 @@
+import Blog from './blog';
+// import Contents from './contents';
+import Footer from './footer';
+import Hero from './hero';
+import Navbar from './navbar';
+import Quotes from './quotes';
+import Services from './services';
+
+export {
+  Blog,
+  Footer,
+  Hero,
+  Navbar,
+  Quotes,
+  Services,
+};
